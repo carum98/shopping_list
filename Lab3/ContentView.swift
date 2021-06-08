75d8f878241c276330e9f072bb32e8048a547860
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-06-07 20:55:08 -0600

    Initial Commit

---
+++ Lab3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab3
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 7/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab3/Lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3App.swift
+//  Lab3
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
